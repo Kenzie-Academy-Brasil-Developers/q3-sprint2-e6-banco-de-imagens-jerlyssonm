a96b214bf4b9bf6eaa30035a33950fd5ee701470
JerlyssonM <jerlyssonm@gmail.com>
2021-12-20 23:20:31 -0300

    modifications according to completed rubric

--- app/kenzie/__init__.py
+++ app/kenzie/__init__.py
@@ -1,12 +1,12 @@
-from flask import Flask,request, send_from_directory
-from .image import *
+from flask import Flask,request,send_from_directory
+from .image import *  #TODAS FUNÇỖES USADAS AQUI ESTÃO SENDO IMPORTADA DE image.
 import os
 
 app = Flask(__name__)
 
 root = os.listdir()
-if "bank" not in root:
-    image.criar_diretorio("./bank")
+if f'{FILES_DIRECTORY}' not in root:
+    create_directory(f"./{FILES_DIRECTORY}")
 
 @app.get('/')
 def home():
@@ -14,21 +14,46 @@ def home():
 
 @app.post('/upload')
 def upload():    
-    recebidos = request.files.items()
-    image.salvar_item(recebidos)
-    return {"message": "Imagens Salvas"}
+    request.files['file'].save('/tmp/file')
+    name = request.files['file'].filename
+    file_length = os.stat('/tmp/file').st_size
+    print(name)
+    if file_length > MAX_SIZE_AUTORIZATION:
+        return {'message': f'Sorry the maximum size per image is {MAX_CONTENT_LENGTH}MB'}
+    elif verification(name):
+        return {'message': 'this name already exists.'}
+    else:
+        recebidos = request.files.items()
+        if  save_item(recebidos) == 'error':
+            return {'message': 'This extension is not allowed.'}
+        else:
+            return {"message": f"Saved images"}
 
 @app.get('/files/<extension>')
 def list_files_by_extension(extension):    
-    return image.listar_por_extensao(extension)
+    return list_by_extension(extension)
 
 @app.get('/files')
 def list_files():
-    return image.listar_todos_items()
+    return list_all_items()
 
-@app.get('/download-zip')
-def download_dir_as_zip():      
-    pass
 @app.get('/download/<file_name>')
-def download():
-    pass
+def download(file_name):
+    extension = file_name[-3::]
+    if not extension in ALLOWED_EXTENSIONS:
+        return {'message': 'file not found.'}, 404
+    return donwload_by_name(file_name,FILES_DIRECTORY,extension)
+
+@app.get('/download-zip/')
+def download_dir_as_zip():
+    extension = request.args.get('file_extension')
+    ratio = int(request.args.get('compression_ratio', 1))
+    path = FILES_DIRECTORY
+    if not extension == None:
+        path = extension
+    try:
+        zipping(extension, ratio)
+        return send_from_directory('/tmp', path=f"{path}.zip", as_attachment=True), 200
+    except:
+        return {'message': f"{extension} file not found"}, 404
+
--- app/kenzie/image.py
+++ app/kenzie/image.py
@@ -1,28 +1,76 @@
+from flask import jsonify, safe_join,send_file
 import os
-from flask import jsonify
 
-def criar_diretorio(name):
+ALLOWED_EXTENSIONS = os.getenv('ALLOWED_EXTENSIONS').split(',')
+FILES_DIRECTORY = os.getenv('FILES_DIRECTORY')
+MAX_CONTENT_LENGTH = int(os.getenv('MAX_CONTENT_LENGTH'))
+MAX_SIZE_AUTORIZATION = MAX_CONTENT_LENGTH * 1048576
+
+def create_directory(name):
+    """criar um diretorio em seu local atual"""
     os.mkdir(name)
 
-def salvar_item(lista):
-    for i in lista:
-        folders = os.listdir('./bank')
+def save_item(list):
+    """salva os items em pastas com o nome de suas extensões"""
+    for i in list:
+        folders = os.listdir(f'./{FILES_DIRECTORY}')
         i_name = i[1].filename
         extension = i_name[-3::].lower()
-        if extension not in folders:
-            criar_diretorio(f'./bank/{extension}')
-            i[1].save(f'./bank/{extension}/{i_name}')
+        if extension in ALLOWED_EXTENSIONS:
+            if extension not in folders:
+                create_directory(f'./{FILES_DIRECTORY}/{extension}')
+                i[1].save(f'./{FILES_DIRECTORY}/{extension}/{i_name}')
+            else:
+                i[1].save(f'./{FILES_DIRECTORY}/{extension}/{i_name}')
         else:
-            i[1].save(f'./bank/{extension}/{i_name}')
+            return 'error'
 
-def listar_por_extensao(extension):
-    out = os.listdir(f'./bank/{extension}/')
+def list_by_extension(extension):
+    """lista os items por extensão"""
+    out = os.listdir(f'./{FILES_DIRECTORY}/{extension}/')
     return jsonify(out)
 
-def listar_todos_items():
-    out = os.listdir(f'./bank/')
-    saida = []
-    for pasta in out:
-        for items in os.listdir(f'./bank/{pasta}/'):
-            saida.append(items)
-    return jsonify(saida)
+def list_all_items():
+    """lista todos os arquivos do diretorio"""
+    current_dirs = os.listdir(f'./{FILES_DIRECTORY}/')
+    out = []
+    for folder in current_dirs:
+        for items in os.listdir(f'./{FILES_DIRECTORY}/{folder}/'):
+            out.append(items)
+    return jsonify(out)
+
+def donwload_by_name(file_name,local,extension):
+    """solicita o download com as informações fornecidas corretamente"""
+    path = os.getcwd()
+    files_path = safe_join(path, local)
+    all_path = safe_join(files_path, extension)
+    files_list = os.listdir(all_path)
+
+    if not file_name in files_list:
+        return {'message': 'file not found.'} , 404
+
+    out = safe_join(all_path, file_name)
+    return send_file(out, as_attachment=True)    
+    
+
+def zipping(extension=None, compression_ratio=1):
+    """comprime o aquivo antes de executar o download do diretorio, solicitando por paramento a extensão e o ratio da comprensão"""
+    if compression_ratio < 0:
+        compression_ratio = 0
+    if compression_ratio > 9:
+        compression_ratio = 9
+    
+    if extension == None:
+        folders = [dir_root.split('/')[-1] for dir_root, dir_files ,files in os.walk(f"{FILES_DIRECTORY}") if len(files) > 0]
+        os.system(f"cd {FILES_DIRECTORY} && zip -r {FILES_DIRECTORY}.zip {' '.join(folders)} && mv {FILES_DIRECTORY}.zip /tmp")
+    else:
+        os.system(f"cd {FILES_DIRECTORY}/{extension.lower()} && zip -{compression_ratio} {extension}.zip * && mv {extension}.zip /tmp")
+
+def verification(file_name):
+    """Verifica se o nome passado por paramentro ja existe no diretorio"""
+    dir_father = os.listdir(f"./{FILES_DIRECTORY}")
+    for i in dir_father:
+        for img in os.listdir(f"./{FILES_DIRECTORY}/{i}"):
+            if img == file_name:
+                return True
+    return False
